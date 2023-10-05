158d4e819fdfb297ae065500c11fbeff83877f44
Kristo <Kangrokristo1@gmail.com>
2023-10-05 13:09:00 +0300

    Add files via upload
    Powershell skript

---
+++ Powershell/yl1.ps1
@@ -0,0 +1,10 @@
+﻿1. $eesnimi = Read-Host "Sisestage oma eesnimi"
+$perenimi = Read-Host "Sisestage oma perenimi"
+
+2. $kasutajanimi = ($eesnimi + "." + $perenimi).ToLower()
+
+3. New-LocalUser -Name $kasutajanimi -Description "$eesnimi $perenimi konto" -Password (ConvertTo-SecureString "Parool1!" -AsPlainText -Force)
+
+4. If ($? -eq $false) {
+Write-Host "Kasutaja loomine ebaõnnestus"
+}
